be2f90047cb6d2350895289497d887a2c6379924
Horton <firetim@126.com>
2015-09-15 11:20:22 +0800

    v1.0.1 横屏+抽屉+Tab

--- app/src/main/java/com/example/horton/toolbar/MainActivity.java
+++ app/src/main/java/com/example/horton/toolbar/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.horton.toolbar;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
+    private ViewPager mViewPager;
+    private TabLayout mTabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +33,63 @@ public class MainActivity extends AppCompatActivity {
 //        toolbar.setLogo(R.mipmap.ic_launcher);  // App logo
         toolbar.setTitle("铛铛");               // Title
 //        toolbar.setSubtitle("Subtitle");        // Sub title
-        setSupportActionBar(toolbar);
+        setSupportActionBar(toolbar);           // 把toolbar当成Actionbar
 
-//        toolbar.setNavigationIcon(R.drawable.ic_menu);
+        final ActionBar ab = getSupportActionBar();
+        if (ab != null) {
+            ab.setHomeAsUpIndicator(R.drawable.ic_menu);    // 设置抽屉图标
+            ab.setDisplayHomeAsUpEnabled(true);
+        }
 
-        Toolbar bottombar = (Toolbar) findViewById(R.id.bottombar);
-//        bottombar.setLogo(R.mipmap.ic_launcher);
-//        bottombar.setTitle("主页");
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.dl_main_drawer);
 
+        setNavigationView();    // 不用抽屉导航时可禁掉此句
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Snackbar comes out", Snackbar.LENGTH_LONG)
+                        .setAction("Action", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Toast.makeText(getApplicationContext(), "Toast comes out", Toast.LENGTH_SHORT).show();
+                            }
+                        }).show();
+            }
+        });
+
+        mViewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager();       // 设置viewpager
+    }
+
+
+
+    private void setNavigationView(){
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nv_main_navigation);
+        if (navigationView != null){
+            navigationView.setNavigationItemSelectedListener(
+                    new NavigationView.OnNavigationItemSelectedListener() {
+                        @Override
+                        public boolean onNavigationItemSelected(MenuItem menuItem) {
+                            menuItem.setChecked(true);
+                            mDrawerLayout.closeDrawers();
+                            return true;
+                        }
+                    }
+            );
+        }
+    }
+
+    private void setupViewPager() {
+        mTabLayout = (TabLayout) findViewById(R.id.tabs);
+        List<String> titles = new ArrayList<>();
+        titles.add("聊天");
+        titles.add("追踪");
+        titles.add("签批");
+        mTabLayout.addTab(mTabLayout.newTab().setText(titles.get(0)));
+        mTabLayout.addTab(mTabLayout.newTab().setText(titles.get(1)));
+        mTabLayout.addTab(mTabLayout.newTab().setText(titles.get(2)));
     }
 
     @Override
