9ca296e7a9196b96ba434ac8aa7a5ece7267cb8b
Horton <firetim@126.com>
2015-09-16 15:39:44 +0800

    v2.0.0 Toolbar only.

---
+++ app/src/main/java/com/horton/www/toolbar/MainActivity.java
@@ -0,0 +1,52 @@
+package com.horton.www.toolbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        toolbar.setLogo(R.mipmap.ic_launcher);  // App logo
+        toolbar.setTitle("铛铛");               // Title
+//        toolbar.setSubtitle("Subtitle");        // Sub title
+        setSupportActionBar(toolbar);
+
+        toolbar.setNavigationIcon(R.drawable.ic_menu);
+
+        Toolbar bottombar = (Toolbar) findViewById(R.id.bottombar);
+//        bottombar.setLogo(R.mipmap.ic_launcher);
+//        bottombar.setTitle("主页");
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
